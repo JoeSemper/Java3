0b52f2e76282eb9ea61daa995abbd75c08d9bac1
JoeSemper <joe.semper@gmail.com>
2020-07-15 12:20:09 +0300

    hw1

---
+++ src/hw1/Box.java
@@ -0,0 +1,41 @@
+package hw1;
+
+import hw1.fruits.Fruit;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class Box<T extends Fruit> {
+
+    private ArrayList<T> list;
+
+    public Box(T[] arr) {
+        this.list = new ArrayList<>(Arrays.asList(arr));
+    }
+
+    public Box() {
+        this.list = new ArrayList<>();
+    }
+
+
+    public void add(T fruit) {
+        list.add(fruit);
+    }
+
+    public float getWeight(){
+        return list.size() * list.get(0).getWeight();
+    }
+
+    public boolean compare(Box<?> anotherBox){
+
+        return getWeight() == anotherBox.getWeight();
+    }
+
+    public void sprinkleTo(Box<T> anotherBox) {
+        for (int i = 0; i <list.size() ; i++) {
+            anotherBox.add(list.get(i));
+            list.set(i, null);
+        }
+    }
+
+}
